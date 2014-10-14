fa5dfbc83e86b3e986c82b246dacba95f4abdf91
Ken M. Haggerty <eurekavalleyco@gmail.com>
2014-10-14 16:16:32 -0400

    Creates FontboxPagingViewController.

---
+++ Fontbox/FontboxPagingViewController.m
@@ -0,0 +1,128 @@
+//
+//  FontboxPagingViewController.m
+//  Fontbox
+//
+//  Created by Ken M. Haggerty on 10/14/14.
+//  Copyright (c) 2014 Eureka Valley Co. All rights reserved.
+//
+
+#pragma mark - // NOTES (Private) //
+
+#pragma mark - // IMPORTS (Private) //
+
+#import "FontboxPagingViewController.h"
+#import "AKDebugger.h"
+
+#pragma mark - // DEFINITIONS (Private) //
+
+@interface FontboxPagingViewController ()
+@property (nonatomic, strong) IBOutlet UIScrollView *scrollView;
+@property (nonatomic, strong) IBOutlet UIView *viewForScrollView;
+- (void)setup;
+- (void)teardown;
+@end
+
+@implementation FontboxPagingViewController
+
+#pragma mark - // SETTERS AND GETTERS //
+
+@synthesize scrollView = _scrollView;
+@synthesize viewForScrollView = _viewForScrollView;
+
+#pragma mark - // INITS AND LOADS //
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    if ([AKDebugger printForMethod:METHOD_NAME logType:AKMethodName methodType:AKSetup rules:RULES_CLASS]) NSLog(@"%s", __PRETTY_FUNCTION__);
+    
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self)
+    {
+        [self setup];
+    }
+    else if ([AKDebugger printForMethod:METHOD_NAME logType:AKWarning methodType:AKSetup rules:RULES_CLASS]) NSLog(@"[WARNING] self is nil for %s", __PRETTY_FUNCTION__);
+    return self;
+}
+
+- (void)awakeFromNib
+{
+    if ([AKDebugger printForMethod:METHOD_NAME logType:AKMethodName methodType:AKSetup rules:RULES_CLASS]) NSLog(@"%s", __PRETTY_FUNCTION__);
+    
+    [super awakeFromNib];
+    [self setup];
+}
+
+- (void)viewDidLoad
+{
+    if ([AKDebugger printForMethod:METHOD_NAME logType:AKMethodName methodType:AKSetup rules:RULES_CLASS]) NSLog(@"%s", __PRETTY_FUNCTION__);
+    
+    [super viewDidLoad];
+}
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    if ([AKDebugger printForMethod:METHOD_NAME logType:AKMethodName methodType:AKSetup rules:RULES_CLASS]) NSLog(@"%s", __PRETTY_FUNCTION__);
+    
+    [super viewWillAppear:animated];
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    if ([AKDebugger printForMethod:METHOD_NAME logType:AKMethodName methodType:AKSetup rules:RULES_CLASS]) NSLog(@"%s", __PRETTY_FUNCTION__);
+    
+    [super viewDidAppear:animated];
+    
+    [self.scrollView setContentSize:self.viewForScrollView.frame.size];
+    NSLog(@"[TEST] contentSize = (%f, %f)", self.scrollView.contentSize.width, self.scrollView.contentSize.height);
+}
+
+- (void)viewWillDisappear:(BOOL)animated
+{
+    if ([AKDebugger printForMethod:METHOD_NAME logType:AKMethodName methodType:AKSetup rules:RULES_CLASS]) NSLog(@"%s", __PRETTY_FUNCTION__);
+    
+    [super viewWillDisappear:animated];
+}
+
+- (void)viewDidDisappear:(BOOL)animated
+{
+    if ([AKDebugger printForMethod:METHOD_NAME logType:AKMethodName methodType:AKSetup rules:RULES_CLASS]) NSLog(@"%s", __PRETTY_FUNCTION__);
+    
+    [super viewDidDisappear:animated];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    if ([AKDebugger printForMethod:METHOD_NAME logType:AKMethodName methodType:AKSetup rules:RULES_CLASS]) NSLog(@"%s", __PRETTY_FUNCTION__);
+    
+    [super didReceiveMemoryWarning];
+}
+
+- (void)dealloc
+{
+    if ([AKDebugger printForMethod:METHOD_NAME logType:AKMethodName methodType:AKSetup rules:RULES_CLASS]) NSLog(@"%s", __PRETTY_FUNCTION__);
+    
+    [self teardown];
+}
+
+#pragma mark - // PUBLIC METHODS //
+
+#pragma mark - // DELEGATED METHODS //
+
+#pragma mark - // OVERWRITTEN METHODS //
+
+#pragma mark - // PRIVATE METHODS //
+
+- (void)setup
+{
+    if ([AKDebugger printForMethod:METHOD_NAME logType:AKMethodName methodType:AKSetup rules:RULES_CLASS]) NSLog(@"%s", __PRETTY_FUNCTION__);
+    
+    [self.scrollView setScrollEnabled:YES];
+    [self.scrollView setPagingEnabled:YES];
+}
+
+- (void)teardown
+{
+    if ([AKDebugger printForMethod:METHOD_NAME logType:AKMethodName methodType:AKSetup rules:RULES_CLASS]) NSLog(@"%s", __PRETTY_FUNCTION__);
+}
+
+@end
